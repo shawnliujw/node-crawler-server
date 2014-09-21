000b1a0fc39e5adda1bc686554adb826a6adf03b
Shawn Liu <shawn.liu@dotter.me>
2014-09-21 20:26:05 +0800

    add unit test for server.js

--- test/lib/server.test.js
+++ test/lib/server.test.js
@@ -1,91 +1,65 @@
-/* jshint expr: true */
+var path = require("path");
+process.env.NODE_CONFIG_DIR = path.join(__dirname, "../../config");
 var sinon = require('sinon');
 var expect = require("chai").expect;
-var clearCache = require('../..//lib/server').clearCache;
+var server = require('../..//lib/server');
 var config = require("config");
-var Promise = require('bluebird');
-var db = require('mongo-bluebird').create(config.scrapecache.db);
+var casperPorts = config.casper.ports;
+var Promise = require("bluebird");
+describe('Operate casper server', function() {
 
-describe('clearCache', function() {
-	var stubs;
-	var detailsCol = db.collection('details');
-	var linksCol = db.collection('search');
-
-	beforeEach(function () {
-		detailsCol.remove({});
+	beforeEach(function() {
+		console.log("before")
+		server.setUp();
 	});
-	
-	function getScript() {
-		return Promise.resolve([
-				{
-					script: 'path/to/script1',
-					url: 'url1',
-					status: true
-				},
-				{
-					script: 'path/to/script3',
-					url: 'url3',
-					status: true
-				}
-			]) 
-	};
 
-	afterEach(function () {
-		stubs.forEach(function(stub) {
-			stub.restore();
+	it("#setUp()", function(done) {
+		Promise.map(casperPorts, function(port) {
+			return server.ping(port)
+					.then(function(res) {
+						console.log(res);
+						expect(res.status).to.equal(true);
+					});
+		})
+		.then(function() {
+			done();
+		})
+		.catch(function() {
+			done();
 		});
 	});
 
-	after(function () {
-		detailsCol.remove({});
-		linksCol.remove({});
+	it("#retrive()", function(done) {
+		server.retrive(casperPorts[0])
+				.then(function(port) {
+					return server.ping(port)
+							.then(function(res) {
+								expect(res.status).to.equal(true);
+							})
+				}).then(function() {
+					done();
+				})
+				.catch(function() {
+					done();
+				});
 	});
-
-	it('removes matched details from db', function (done) {
-		detailsCol.insert([{
-			url: 'url1',
-			script: 'script1',
-		},{
-			url: 'url2',
-			script: 'script2'
-		},{
-			url: 'url3',
-			script: 'script3'
-		}])
-			.then (function () {
-				return clearCache('details', ['url1', 'url2', 'url3'], 'en_gb', 'amazon.com');
-			}).then(function () {
-				return detailsCol.find()
-					.then(function (details) {
-						expect(details).to.have.length(1);
-						expect(details[0].url).to.equal('url2');
-					});
-			}).then(done, done);
-	});
-
-	it('removes matched links from db', function (done) {
-		linksCol.insert([{
-			url: 'url1',
-			script: 'script1',
-		},{
-			url: 'url2',
-			script: 'script2'
-		},{
-			url: 'url3',
-			script: 'script3'
-		}])
-			.then (function () {
-				return clearCache('links', ['url1', 'url2', 'url3'], 'en_gb', 'amazon.com');
-			}).then(function () {
-				return linksCol.find()
-					.then(function (details) {
-						expect(details).to.have.length(1);
-						expect(details[0].url).to.equal('url2');
-					});
-			}).then(done, done);
-	});
-});
-
-describe("scrape information",function(){
 	
+	it("#releaseAll()",function(done){
+		server.releaseAll()
+				.then(function(){
+					return Promise.map(casperPorts, function(port) {
+								return server.ping(port)
+										.then(function(res) {
+											console.log(res);
+											expect(res.status).to.equal(false);
+										});
+							})
+				}).then(function() {
+							done();
+						})
+				.catch(function() {
+					done();
+				});
+	});
 });
+
