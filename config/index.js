f9cc5e37d5a071ea5c90bf216f53943b87854d25
shawnLiujianwei <shawn.liu@dotter.me>
2014-09-23 16:28:29 +0800

    tidy up log output

--- config/index.js
+++ config/index.js
@@ -6,26 +6,27 @@ var _ = require('lodash');
 var fs = require("fs");
 
 function requiredProcessEnv(name) {
-    if (!process.env[name]) {
-        throw new Error('You must set the ' + name + ' environment variable');
-    }
-    return process.env[name];
+	if (!process.env[name]) {
+		throw new Error('You must set the ' + name + ' environment variable');
+	}
+	return process.env[name];
 }
 
 // All configurations will extend these options
 // ============================================
 var all = {
-    "env": process.env.NODE_ENV || "development"
+	"env": process.env.NODE_ENV || "development"
 };
 
 // Export the config object based on the NODE_ENV
 // ==============================================
 function merge() {
-    var res = _.merge(all,config);
-    res = _.merge(res,require("./development"));
-    if(all.evn === "production" && fs.existsSync('./' + all.env + '.js')) {
-        res = _.merge(res,require('./' + all.env + '.js'));
-    }
-    return res;
+	var res = _.merge(all, config);
+	if (all.evn === "production" && fs.existsSync('./' + all.env + '.js')) {
+		res = _.merge(require('./' + all.env + '.js'),res);
+	} else {
+		res = _.merge(require("./development"), res);
+	}
+	return res;
 }
 module.exports = merge();
